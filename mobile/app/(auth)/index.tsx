144535b972d80c520ffd740d275c9d3ab6d05b62
Yogesh Sharma <nitya.upmanyu@gmail.com>
2026-01-01 08:52:38 +0530

    exxhange branch fix

---
+++ mobile/app/(auth)/index.tsx
@@ -0,0 +1,15 @@
+
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View
+     className="flex flex-1 items-center justify-center bg-green-200"
+    >
+      <Text className="text-7xl text-orange-500 rounded font-semibold">
+        Hello Auth Screen
+      </Text>
+    </View>
+  );
+}
